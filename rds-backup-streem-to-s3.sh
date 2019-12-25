e2d6cd0b5e62f8d0831d04e1dd8a769c835d4bbc
alexg84 <galperin25@gmail.com>
2019-12-25 17:39:33 +0200

    Update rds-backup-streem-to-s3.sh

--- rds-backup-streem-to-s3.sh
+++ rds-backup-streem-to-s3.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+TIMEZONE=Europe/Amsterdam
+TODAY=$(TZ=$TIMEZONE date +%d-%m-%Y)
+
+#ENDPOINT=""
+#RDS_NAME=""
+#DOWNLOAD_FOLDER=""
+#USER=""
+#PASSWORD=""
+#S3-PATH=""
 
 
-
-
-
-mysqldump -u ${USER} -p${PASSWORD} -h ${ENDPOINT} --single-transaction --routines --events --triggers --all-databases | aws s3 cp - "s3://${}/${RDS_NAME}-${TODAY}.sql"
+mysqldump -u ${USER} -p${PASSWORD} -h ${ENDPOINT} --single-transaction --routines 
+--events --triggers --all-databases | aws s3 cp - "s3://${S3-PATH}/${RDS_NAME}-${TODAY}.sql"
